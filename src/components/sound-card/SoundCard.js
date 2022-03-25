f446ddb446e9e0c3ddeae2fcc29c3e9bd24ea20c
i-naeem <inaeem@protonmail.com>
2022-03-25 18:22:29 +0500

    :sparkles: make card clickable

--- src/components/sound-card/SoundCard.js
+++ src/components/sound-card/SoundCard.js
@@ -1,18 +1,35 @@
 import styles from './sound-card.module.css';
 import icons from '../../assets/icons';
+import { useEffect, useRef, useState } from 'react';
 
 const SoundCard = (props) => {
   const { title, url } = props;
+  const [isPlaying, setIsPlaying] = useState(false);
 
   const Icon = icons[title];
 
+  const audioRef = useRef();
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current.play();
+      return;
+    }
+
+    audioRef.current.pause();
+  }, [isPlaying]);
+
   return (
-    <section className={styles.card} role="button">
+    <section
+      onClick={() => setIsPlaying(!isPlaying)}
+      className={`${styles.card} ${isPlaying && styles.active}`}
+      role="button"
+    >
       <h5 className={styles.title}>{title}</h5>
       <div className={styles.iconWrapper}>
         {Icon && <Icon className={styles.icon} />}
       </div>
-      <audio>
+      <audio loop ref={audioRef}>
         <source src={url} />
       </audio>
     </section>
